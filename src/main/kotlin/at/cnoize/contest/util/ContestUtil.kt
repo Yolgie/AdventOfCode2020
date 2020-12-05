099220f827de8a825acb9ccce8a0d811c5854379
Matthias Holzinger <Matthias.Holzinger@cloudflight.io>
2020-12-05 06:57:45 +0100

    day5

--- src/main/kotlin/at/cnoize/contest/util/ContestUtil.kt
+++ src/main/kotlin/at/cnoize/contest/util/ContestUtil.kt
@@ -11,6 +11,18 @@ fun <RETURN_TYPE> String.asResource(work: (String) -> RETURN_TYPE): RETURN_TYPE 
 fun Int.zeroPad(length: Int): String = this.toString().padStart(length, '0')
 
 fun <T> List<T>.toPair(): Pair<T, T> {
-    require (this.size == 2) { "List is not of length 2!" }
+    require(this.size == 2) { "List is not of length 2!" }
     return Pair(this[1], this[2])
+}
+
+fun <T> List<T>.minAndMax(): Pair<T, T> where T : Comparable<T> {
+    require(this.isNotEmpty()) { "List is empty" }
+    return Pair(
+        this.minOrNull() ?: throw IllegalArgumentException("No minimum found"),
+        this.maxOrNull() ?: throw IllegalArgumentException("No maximum found")
+    )
+}
+
+fun Pair<Int, Int>.toRange(): IntRange {
+    return this.first..this.second
 }
