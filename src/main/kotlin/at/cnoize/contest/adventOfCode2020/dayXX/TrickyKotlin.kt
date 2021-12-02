fa61caad41538065989d5a03850340bf97192af5
Matthias Holzinger <Matthias.Holzinger@cloudflight.io>
2021-12-03 00:59:40 +0100

    leftover stuff never committed

---
+++ src/main/kotlin/at/cnoize/contest/adventOfCode2020/dayXX/TrickyKotlin.kt
@@ -0,0 +1,9 @@
+
+fun main() {
+    val nearlyLeet: Int? = 1330
+    val leet = nearlyLeet ?: 0 + 7 // -> 1330
+    val theAnswer = -1.inc() // - ( 1.inc() )
+    + leet - 1295 // unary +
+
+    print(theAnswer)
+}
