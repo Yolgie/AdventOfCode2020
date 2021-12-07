7e7d8f5a09ece722cd56ab50b872b1910ac694d4
Matthias Holzinger <Matthias.Holzinger@cloudflight.io>
2021-12-07 12:11:48 +0100

    removed old session cookie

--- src/main/kotlin/at/cnoize/contest/util/InputDownloader.kt
+++ src/main/kotlin/at/cnoize/contest/util/InputDownloader.kt
@@ -12,7 +12,7 @@ fun main() {
     val request = Request.Builder()
         .addHeader(
             "Cookie",
-            "session=53616c7465645f5f6c1722965cee6ffa4046cdca296432703083dd5dc87e7d4610e62d8bafe7da50c515fa459e329423"
+            "session=**REMOVED**"
         )
         .url(URL)
         .get()
